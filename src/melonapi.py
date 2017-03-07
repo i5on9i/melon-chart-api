38d0c3b57b360bc98036c4349ad5dfdb0376b6f8
i5on9i <gaedduck@gmail.com>
2017-03-08 00:10:14 +0900

    - add method for adjusting the start date

--- src/melonapi.py
+++ src/melonapi.py
@@ -16,7 +16,7 @@ class MelonChart(object):
     def __init__(self):
         self.tree = None
 
-    def getChart(self, sdate, edate):
+    def getChart(self, sdate):
         """
             Retrieve the chart list between {sdate} and {edate}
             Then, set {self.tree}
@@ -24,8 +24,9 @@ class MelonChart(object):
             :param sdate datetime
             :param edate datetime
         """
-        sdateStr = sdate.strftime("%Y%m%d")
-        edateStr = edate.strftime("%Y%m%d")
+        adjSdate, adjEdate = self._adjustedDate(sdate)
+        sdateStr = adjSdate.strftime("%Y%m%d")
+        edateStr = adjEdate.strftime("%Y%m%d")
         dayVal = "{sdate}^{edate}"\
                 .format(sdate=sdateStr, edate=edateStr)
         
@@ -34,8 +35,8 @@ class MelonChart(object):
         options = {
             "chartType" : ChartType.WEEK,
             "age" : ageVal,
-            "year" : sdate.year,
-            "mon" : "%02d"%(sdate.month,),
+            "year" : adjSdate.year,
+            "mon" : "%02d"%(adjSdate.month,),
             "day" : dayVal, # "20160307%5E20160313"
             "classCd" : "DP0100",
             "startDay" : sdateStr,
@@ -52,6 +53,39 @@ class MelonChart(object):
         self.tree   = etree.parse(StringIO(content), parser)
 
         return self
+
+    def _adjustedDate(self, sdate):
+        """
+            :return 
+                Returns the first Monday when the sdate is later than 2012-08-06,
+                or the first Sunday when the sdate is earlier than 2012-08-06
+                when the sdate is 2012-08-06, 2012-08-13 is returned
+
+            Aug. 2012
+            |Sun.| ...
+             <------------------------------|
+            | 5  | 6  | 7 | 8 | 9 | 10 | 11 |
+            | 12 | 13 |-->
+            # 2012-08-05 -> 2012-08-13
+        """
+        weekday = sdate.weekday()
+        if sdate > datetime(2012, 8, 6) and weekday != 0:
+            # After the start-date is 2012-08-06, 
+            # the weekday of the start date should be Monday
+            sdate += timedelta(days=0-weekday+7) # the next first Mon.
+
+        elif sdate == datetime(2012, 8, 6):
+            sdate = datetime(2012, 8, 13)
+
+        elif sdate < datetime(2012, 8, 6) and weekday != 6:
+            # Before the start-date is 2012-08-06, 
+            # the Sun. is the start day of the week chart
+            sdate += timedelta(days=6-weekday) # the next first Mon.
+
+        return sdate, sdate+timedelta(days=6)
+
+
+
 
 
     def _saveAsFile(self, content):
@@ -76,22 +110,29 @@ class MelonChart(object):
 def main():
     melonSearchChartUrl = "http://www.melon.com/chart/search/list.htm"
 
+    import time
 
-    # sdate = datetime(2016, 3, 7)
-    # edate = datetime(2016, 3, 13)
-    sdate = datetime(2016, 5, 23)
-    edate = datetime(2016, 5, 29)
+    # 2012-08-05 -> 2012-08-13
+    sdate = datetime(2016, 2, 28)
+    edate = datetime(2017, 2, 27)
+    # sdate = datetime(2016, 5, 23)
+    # edate = datetime(2016, 5, 29)
     tdelta = timedelta(days=7)
+    tdelta2 = timedelta(days=6)
     
     mchart = MelonChart()
-    while True:
+    csdate = sdate
+    cedate = csdate + tdelta2
+    while cedate <= edate:
 
-        rank = mchart.getChart(sdate, edate).getRank("벚꽃 엔딩")
-        print("sdate = {sdate}".format(sdate=sdate))
-        print(rank)
+        rank = mchart.getChart(csdate).getRank("벚꽃 엔딩")
+        print("{sdate},{rank}".format(sdate=csdate, rank=rank))
 
-        sdate = sdate + tdelta
-        edate = edate + tdelta
+        csdate = csdate + tdelta
+        cedate = csdate + tdelta2
+        
+
+
 
     
 
